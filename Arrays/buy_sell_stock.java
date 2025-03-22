2cd7cb51e9f96eba0d904283901d95a138ba81a4
Manan Mishra <146760182+MananHere576@users.noreply.github.com>
2025-03-22 09:35:23 +0530

    Create buy_sell_stock.java

---
+++ Arrays/buy_sell_stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=prices[0];
+        int p=0;
+        int diff=0;
+        int ndiff=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            min=prices[i];
+
+            ndiff=prices[i]-min;
+            if(ndiff>diff)
+            diff=ndiff;
+        }
+        return diff;
+    }
+}
